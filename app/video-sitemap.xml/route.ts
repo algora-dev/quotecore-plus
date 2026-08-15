0d61703d414e2606f6b8a94aaeda198f25a993e7
Gavin (QuoteCore+ Agent) <gavin@quotecore.local>
2026-08-15 19:04:27 +0100

    seo: fix video sitemap structure + allow /docs help-centre URLs in robots

--- app/video-sitemap.xml/route.ts
+++ app/video-sitemap.xml/route.ts
@@ -3,18 +3,26 @@ import { VIDEOS, PAGE_VIDEOS } from "@/lib/videos";
 const SITE_URL = "https://quote-core.com";
 
 export async function GET() {
-  const entries = PAGE_VIDEOS.map(({ pagePath, videoKey }) => {
-    const v = VIDEOS[videoKey];
+  const videosByPage = new Map<string, Array<(typeof VIDEOS)[keyof typeof VIDEOS]>>();
+  for (const { pagePath, videoKey } of PAGE_VIDEOS) {
+    const video = VIDEOS[videoKey];
+    const current = videosByPage.get(pagePath) ?? [];
+    current.push(video);
+    videosByPage.set(pagePath, current);
+  }
+
+  const entries = Array.from(videosByPage.entries()).map(([pagePath, videos]) => {
     const pageUrl = `${SITE_URL}${pagePath}`;
-    return `  <url>
-    <loc>${pageUrl}</loc>
-    <video:video>
+    const videoXml = videos.map((v) => `    <video:video>
       <video:thumbnail_loc>${v.thumbnail}</video:thumbnail_loc>
       <video:title><![CDATA[${v.title}]]></video:title>
       <video:description><![CDATA[${v.description}]]></video:description>
       <video:player_loc>https://www.youtube-nocookie.com/embed/${v.id}</video:player_loc>
       <video:upload_date>${v.uploadDate}</video:upload_date>
-    </video:video>
+    </video:video>`).join("\n");
+    return `  <url>
+    <loc>${pageUrl}</loc>
+${videoXml}
   </url>`;
   }).join("\n");
 
